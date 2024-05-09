bacf559390fc7e54ffbcab7414bd4c931f4de37a
Eduardo <etrevino1@hotmail.com>
2024-05-09 17:08:40 -0600

    still initial =P

--- HelloWebWorld/obj/Debug/net8.0/HelloWebWorld.AssemblyInfo.cs
+++ HelloWebWorld/obj/Debug/net8.0/HelloWebWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWebWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d430227a564ed5ff900535c2fa38da6ca0cc1dcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWebWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWebWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
